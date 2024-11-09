0603859ffa49fbfbe7b51af1c1d8195f68e2f25f
Marchenko189 <marcenkoo189@gmail.com>
2024-11-09 17:14:53 +0200

    homework 2

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,16 @@
-function getElementWidth(content, padding, border,) {
-    const borderBox = Number.parseFloat(content) + 2 * Number.parseFloat(padding) + 2 * Number.parseFloat(border);
-    return borderBox;
-};
+// function checkForSpam(message) {
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+// // Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
+//     const banWord = message.toLowerCase();
+
+// // Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
+//     return banWord.includes("spam") || banWord.includes("sale");
+// }  
+
+// console.log(checkForSpam("Latest technology news")); // false
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+// console.log(checkForSpam("Get best sale offers now!")); // true
+// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+// console.log(checkForSpam("Trust me, this is not a spam message")); // true
+// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
